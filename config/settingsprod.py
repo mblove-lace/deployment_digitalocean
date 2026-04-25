15ac6b3b8efde1874df38a9f5d6fa4b28b7de236
M Bhattacherjee <mblovelace15@gmail.com>
2026-04-25 11:53:29 +0530

    Naavyaa.in works as well

--- config/settingsprod.py
+++ config/settingsprod.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.naavyaa.in']
+ALLOWED_HOSTS = ['naavyaa.in', 'www.naavyaa.in']
 
 
 # Application definition
